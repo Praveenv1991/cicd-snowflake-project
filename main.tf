ebe41f92d7f23f589169e54f4b61af02c0a2aa7a
Praveen <vpraveenmba2016@gmail.com>
2026-04-08 21:53:32 +0530

    trigger cicd pipeline

--- main.tf
+++ main.tf
@@ -1,17 +1,31 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.98"
+      source  = "snowflakedb/snowflake"
+      version = "~> 1.0"
     }
   }
 }
 
 provider "snowflake" {
-  account  = "QNNPHFX-WN06484"
-  username = "PRAVEENV"
-  password = "Advaith@202666"
-  role     = "ACCOUNTADMIN"
+  organization_name = "QNNPHFX"
+  account_name      = "WN06484"
+  user              = var.username
+  password          = var.password
+  role              = "ACCOUNTADMIN"
+}
+
+variable "account" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
 }
 
 resource "snowflake_database" "demo_db" {
