c6a2cddc7532b1cec6942db0770e701406f9b5e4
Praveen <vpraveenmba2016@gmail.com>
2026-04-08 22:23:41 +0530

    add schema and customers table

--- main.tf
+++ main.tf
@@ -31,3 +31,29 @@ variable "password" {
 resource "snowflake_database" "demo_db" {
   name = "DEMO_DB_V2"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "PUBLIC"
+}
+
+resource "snowflake_table" "customer_table" {
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.demo_schema.name
+  name     = "CUSTOMERS"
+
+  column {
+    name = "ID"
+    type = "NUMBER"
+  }
+
+  column {
+    name = "NAME"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "CITY"
+    type = "VARCHAR"
+  }
+}
